8276329f9e44cf6f9546ae43cd984b5ab868fdd9
Junaid PV <junaid@detlus.com>
2020-04-22 19:26:33 +0530

    Basic working implementation of the module.

---
+++ src/EventSubscriber/BlockComponentRenderArraySubscriber.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\layout_target_styles\EventSubscriber;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\layout_builder\LayoutBuilderEvents;
+
+/**
+ * Class BlockComponentRenderArraySubscriber.
+ */
+class BlockComponentRenderArraySubscriber implements EventSubscriberInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Drupal\Core\Config\ConfigFactoryInterface definition.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * BlockComponentRenderArraySubscriber constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   Access configuration.
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, ConfigFactoryInterface $config_factory) {
+    $this->entityTypeManager = $entityTypeManager;
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    // Layout Builder also subscribes to this event to build the initial render
+    // array. We use a higher weight so that we execute after it.
+    $events[LayoutBuilderEvents::SECTION_COMPONENT_BUILD_RENDER_ARRAY] = ['onBuildRender', 50];
+    return $events;
+  }
+
+  /**
+   * Add each component's block styles to the render array.
+   *
+   * @param \Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent $event
+   *   The section component render event.
+   */
+  public function onBuildRender(SectionComponentBuildRenderArrayEvent $event) {
+    $build = $event->getBuild();
+    // This shouldn't happen - Layout Builder should have already created the
+    // initial build data.
+    if (empty($build)) {
+      return;
+    }
+
+    if (!isset($build['#attributes']['class']) || !is_array($build['#attributes']['class'])) {
+      $build['#attributes']['class'] = [];
+    }
+
+    $layout_target_styles_html_classes = $event->getComponent()->get('layout_target_styles_html_classes');
+    if ($layout_target_styles_html_classes) {
+      $build['#attributes']['class'] = array_merge($build['#attributes']['class'], [$layout_target_styles_html_classes]);
+    }
+
+    $layout_target_styles_html_id = $event->getComponent()->get('layout_target_styles_html_id');
+    if ($layout_target_styles_html_id) {
+      $build['#attributes']['id'] = $layout_target_styles_html_id;
+    }
+
+    $event->setBuild($build);
+  }
+
+}
